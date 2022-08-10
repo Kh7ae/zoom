6f906e4f626e3ce242853f98b40c818e84892cb4
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-10 17:31:14 +0900

    Update

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socekt = new WebSocket(`ws://${window.location.host}`);
+
+socekt.addEventListener("open", () => {
+    console.log("Connected to Server ✅")
+});
+
+socekt.addEventListener("message", (message) => {
+    console.log("New message: ", message.data)
+});
+
+socekt.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌")
+});
+
+setTimeout(() => {
+    socekt.send("Hello from the Browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,23 @@ const app = express();
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on https://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅")
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    })
+    // https://velog.io/@plate0113/JS-Buffer-68-65-6c-6c-6f-20-66-72-6f-6d-20-74-68-65-20-62-72-6f-77-73-65-72-73-%ED%98%84%EC%83%81-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95
+    // utf8로 변환하지 않고 그냥 message를 받았더니 이상한 Buffer 어쩌고를 받았다 ..
+    socket.send("Hello!");
+});
+
+server.listen(3000, handleListen);
