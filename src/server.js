663961f1120700f330cad39ffe70ee195767598b
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-14 23:37:02 +0900

    Update

--- src/server.js
+++ src/server.js
@@ -13,47 +13,5 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname);
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => 
-            socket.to(room).emit("bye", socket.nickname));
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on("nickname", (nickname) => socket["nickname"] = nickname);
-});
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () => console.log(`Listening on httpL//localhost:3000`);
 httpServer.listen(3000, handleListen);
-
-// const wss = new WebSocket.Server({ server });
-// const sockets = [];
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser ✅")
-//     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch(message.type) {
-//             case "new_message":
-//                 const msgEncoding = message.payload.toString("utf8");
-//                 const nicknameEncoding = socket.nickname.toString("utf8");
-//                 sockets.forEach((aSocket) => aSocket.send(
-//                     `${nicknameEncoding}: ${msgEncoding}` ));
-//             case "nickname":
-//                 socket["nickname"] = message.payload.toString("utf8");
-//         }
-//     });
-// });
